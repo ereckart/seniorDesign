498569b9dcbb898b09b7c55171c0191a50f8f148
Kevin Thomas <thomaskevinlee@gmail.com>
2018-11-10 13:02:42 -0500

    Working user registration and sms code sending

--- backend/lib/login.js
+++ backend/lib/login.js
@@ -32,9 +32,76 @@ function login(connection, phone, password, func) {
     }
   });
 }
+
+
+function register(connection, first, last, phone, password, func) {
+  var hash = cryptr.encrypt(password);
+  if (first.length == 0) {
+    func('error_first', null);
+  } else if (last.length == 0) {
+    func('error_last',null);
+  } else if (phone.length != 10) {
+    func('error_phone',null);
+  } else if (password.length <= 7) {
+    func('error_password',null);
+  } else {
+    connection.query("SELECT * FROM users WHERE phone = ?", [phone], function (error, results, fields) {
+      if (error != null) {
+        func('error_misc',null);
+      } else if (results.length > 0) {
+        func('error_phone_registered', null);
+      } else {
+        counter.uniq(connection, 'pending', function (error, count) {
+          if (error != null) {
+            func(error, null);
+          } else {
+            var user_id = Math.round(Math.random()*899999+100000)+''+count;
+            var private_key = 'TRT_'+count+'_'
+                              +Math.round(Math.random()*899999+100000)+''
+                              +Math.round(Math.random()*899999+100000)+''
+                              +Math.round(Math.random()*899999+100000)+''
+                              +Math.round(Math.random()*899999+100000);
+            var sms_key = Math.round(Math.random()*899999+100000)
+            connection.query("INSERT INTO `users_pending`(`user_id`,`private_key`,`sms_key`, `first`, `last`, `phone`, `password`) VALUES (?,?,?,?,?,?,?)", [user_id,private_key,sms_key,first,last,phone,hash], function (error, results, fields) {
+              if (error != null) {
+                func('error_misc', null);
+              } else {
+                send_sms_verification(connection, private_key, function(error) {
+                  if (error != null) {
+                    func('error_sending_sms', null);
+                  } else {
+                    func(null,private_key);
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+}
+
+function send_sms_verification(connection, key, func) {
+	connection.query("SELECT * FROM users_pending WHERE private_key = ?", [key], function (error, results, fields) {
+		if (error != null) {
+      func('error_misc');
+		} else {
+			var phone = results[0].phone;
+			var code = results[0].sms_key;
+			sms.send(phone,"Waitr+Verification+Code:+"+code);
+      func(null);
+		}
+	});
+}
+
+
+exports.login = login;
+exports.register = register;
+
 sql.pool.getConnection(function(err, connection) {
-  login(connection, '9546955202', '12345678', function(error, pat) {
+  register(connection, 'Kevin', 'Thomas', '9546955202', '12345678', function(error, key) {
     console.log(error);
-    console.log(pat);
+    console.log(key);
   })
 });
