e41179007eb4bddcf7bbabb3c1c60d66f76cb984
Kevin Thomas <thomaskevinlee@gmail.com>
2018-11-22 03:59:44 -0500

    update login API to use auto increment in DB

--- backend/lib/login.js
+++ backend/lib/login.js
@@ -17,25 +17,7 @@ function login(connection, phone, password, func) {
       if (cryptr.decrypt(hash) != password) {
         func('error_password',null);
       } else {
-        counter.uniq(connection, 'pat', function (error, count) {
-          if (error != null) {
-            func(error,null);
-          } else {
-            counter.token(connection, 'pat', 'PAT', 75, function(err, pat) {
-              if (error != null) {
-                func(error, null);
-              } else {
-                connection.query("INSERT INTO `users_pat`(`pat`, `user_id`) VALUES (?,?)", [pat,user_id], function (error, results, fields) {
-                  if (error != null) {
-                    func(error,null);
-                  } else {
-                    func(null,pat);
-                  }
-                });
-              }
-            });
-          }
-        });
+        create_pat(connection, user_id, func);
       }
     }
   });
@@ -59,28 +41,18 @@ function register(connection, first, last, phone, password, func) {
       } else if (results.length > 0) {
         func('error_phone_registered', null);
       } else {
-        counter.uniq(connection, 'user', function (error, user_id) {
+        var sms_key = Math.round(Math.random()*899999+100000);
+        var pk2 = counter.rand_str(40);
+        connection.query("INSERT INTO `users_pending`(`private_key`,`sms_key`, `first`, `last`, `phone`, `password`) VALUES (?,?,?,?,?,?); SELECT LAST_INSERT_ID() AS pk1;", [pk2,sms_key,first,last,phone,hash], function (error, results, fields) {
           if (error != null) {
             func(error, null);
           } else {
-            var sms_key = Math.round(Math.random()*899999+100000);
-            counter.token(connection, 'pkey', 'PKEY', 75, function(err, private_key) {
+            var private_key = results[1][0].pk1+'_'+pk2;
+            send_sms_verification(connection, private_key, function(error) {
               if (error != null) {
-                func(error, null);
+                func('error_sending_sms', null);
               } else {
-                connection.query("INSERT INTO `users_pending`(`user_id`,`private_key`,`sms_key`, `first`, `last`, `phone`, `password`) VALUES (?,?,?,?,?,?,?)", [user_id,private_key,sms_key,first,last,phone,hash], function (error, results, fields) {
-                  if (error != null) {
-                    func('error_misc', null);
-                  } else {
-                    send_sms_verification(connection, private_key, function(error) {
-                      if (error != null) {
-                        func('error_sending_sms', null);
-                      } else {
-                        func(null,private_key);
-                      }
-                    });
-                  }
-                });
+                func(null,private_key);
               }
             });
           }
@@ -91,61 +63,85 @@ function register(connection, first, last, phone, password, func) {
 }
 
 function send_sms_verification(connection, key, func) {
-	connection.query("SELECT * FROM users_pending WHERE private_key = ?", [key], function (error, results, fields) {
-		if (error != null) {
-      func('error_misc');
-		} else {
-			var phone = results[0].phone;
-			var code = results[0].sms_key;
-			sms.send(phone,"Waitr+Verification+Code:+"+code);
-      func(null);
-		}
-	});
+  var split_key = key.split('_');
+  if (split_key.length != 2) {
+    func('error_private_key');
+  } else {
+    var v_id = split_key[0];
+    var p_key = split_key[1];
+  	connection.query("SELECT * FROM users_pending WHERE verify_id = ? AND private_key = ?", [v_id, p_key], function (error, results, fields) {
+  		if (error != null) {
+        func('error_misc');
+  		} else {
+  			var phone = results[0].phone;
+  			var code = results[0].sms_key;
+  			sms.send(phone,"Waitr+Verification+Code:+"+code);
+        func(null);
+  		}
+  	});
+  }
 }
 
-function verify(connection, trt, code, func) {
-  connection.query("SELECT * FROM users_pending WHERE private_key = ? AND attempts > 0", [trt], function (error, results, fields) {
-    if (error != null) {
-      func('error_misc',null);
-    } else if (results.length == 0) {
-      func('error_trt',null);
-    } else {
-      var actual_code = results[0].sms_key;
-      var user_id = results[0].user_id;
-      var user_phone = results[0].phone;
-      var user_password = results[0].password;
-      var user_first = results[0].first;
-      var user_last = results[0].last;
-      if (actual_code == code) {
-        counter.token(connection, 'pat', 'PAT', 75, function(err, pat) {
-          if (error != null) {
-            func(error, null);
-          } else {
-            connection.query("INSERT INTO `users_pat`(`pat`, `user_id`) VALUES (?,?)", [pat,user_id], function (error, results, fields) {
-              if (error != null) {
-                func(error,null);
-              } else {
-                connection.query("INSERT INTO `users`(`user_id`, `phone`, `password`, `first`, `last`) VALUES (?,?,?,?,?)", [user_id,user_phone,user_password,user_first, user_last], function (error, results, fields) {
-                  if (error != null) {
-                    func(error,null);
-                  } else {
-                    connection.query("DELETE FROM users_pending WHERE private_key = ? OR phone = ?", [trt,user_phone], function (error, results, fields) {
-                      if (error != null) {
-                      } else {
-                        func(null,pat);
-                      }
-                    });
-                  }
-                });
-              }
-            });
-          }
-        });
+function verify(connection, key, code, func) {
+  var split_key = key.split('_');
+  if (split_key.length != 2) {
+    func('error_private_key', null);
+  } else {
+    var v_id = split_key[0];
+    var p_key = split_key[1];
+    connection.query("SELECT * FROM users_pending WHERE private_key = ? AND verify_id = ? AND attempts > 0", [p_key, v_id], function (error, results, fields) {
+      if (error != null) {
+        func('error_misc',null);
+      } else if (results.length == 0) {
+        func('error_private_key',null);
       } else {
-        connection.query("UPDATE users_pending SET attempts = attempts-1 WHERE private_key = ?", [trt], function (error, results, fields) {
-            func('error_code',null);
-        });
+        var actual_code = results[0].sms_key;
+        var user_id = results[0].user_id;
+        var user_phone = results[0].phone;
+        var user_password = results[0].password;
+        var user_first = results[0].first;
+        var user_last = results[0].last;
+        if (actual_code == code) {
+          connection.query("INSERT INTO `users`(`phone`, `password`, `first`, `last`) VALUES (?,?,?,?);"+
+                            " DELETE FROM users_pending WHERE phone = ?;"+
+                            " SELECT LAST_INSERT_ID() AS user_id;",
+                             [user_phone,user_password,user_first, user_last,user_phone],
+                                            function (error, results, fields) {
+            if (error != null) {
+              func('error_misc',null);
+            } else {
+              var user_id = results[2][0].user_id;
+              create_pat(connection, user_id, function(error, pat) {
+                if (error != null) {
+                  func(error,null);
+                } else {
+                  func(null,pat);
+                }
+              });
+            }
+          });
+        } else {
+          connection.query("UPDATE users_pending SET attempts = attempts-1 WHERE private_key = ? AND verify_id = ?", [p_key, v_id], function (error, results, fields) {
+              func('error_code',null);
+          });
+        }
       }
+    });
+  }
+}
+
+function create_pat(connection, user_id, func) {
+  var token = counter.rand_str(40);
+  connection.query("INSERT INTO `users_pat`(`token`, `user_id`) VALUES (?,?);"+
+                    "UPDATE `users_pat` SET `token` = concat(LAST_INSERT_ID(),?) WHERE `pat_id` = LAST_INSERT_ID();"+
+                    "SELECT `token` FROM `users_pat` WHERE `pat_id` = LAST_INSERT_ID();",
+                     [token, user_id, token], function(error, results, fields) {
+    if (error != null) {
+      console.log(error);
+      func('error_issuing_pat',null);
+    } else {
+      console.log(results);
+      func(null, results[2][0].token);
     }
   });
 }
